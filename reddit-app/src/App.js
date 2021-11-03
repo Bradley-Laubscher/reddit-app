71c92c454b092355c43e6c9104196f43c62a5689
Bradley-Laubscher <bradleylaubscher@gmail.com>
2021-11-03 08:59:39 +0200

    Initial

---
+++ reddit-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className='App-logo' src='https://www.redditinc.com/assets/images/site/reddit-logo.png' alt='Reddit-logo'/>
+        <h1>
+          <span style={{color: 'red'}}>Reddit</span>Minimal
+        </h1>
+      </header>
+      <div className='App-body'>
+        {/* </Component1>
+        </Component2> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
