ebcf5da4a4702c03d581947c60e6fe8d64d15b14
Bradley-Laubscher <bradleylaubscher@gmail.com>
2021-11-29 12:11:21 +0200

    Change Api fetch method (Now working) fror reddit articles

--- reddit-app/src/App.js
+++ reddit-app/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
-import React from 'react';
-import { GetPosts } from './components/body/getPosts';
+import React, { useState, useEffect } from 'react';
+import { Article } from './components/body/article';
+// import GetPosts from './components/body/getPosts';
 
 
 function App() {
+
+  const [articles, setArticles] = useState([]);
+  const [subreddit, setSubreddit] = useState('webdev');
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/" + subreddit + ".json").then(res => {
+      if (res.status !== 200) {
+        console.log('Error')
+        return;
+      }
+
+      res.json().then(data => {
+        if (data != null) {
+          setArticles(data.data.children);
+        }
+      });
+    })
+  }, [subreddit]);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,8 +34,13 @@ function App() {
         </h1>
       </header>
       <div className='App-body'>
-        <GetPosts/>
-        
+      <input type='text' className='input' value={subreddit} onChange={ e => setSubreddit(e.target.value)}/>
+        {/* <GetPosts/> */}
+        <div className='articles'>
+                {
+                    (articles != null) ? articles.map((article, index) => <Article key={index} article={article.data} />) : ''
+                }
+            </div>
       </div>
     </div>
   );
